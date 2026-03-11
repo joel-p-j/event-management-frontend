8dcd828031eaee630d3e0846317edff006be5a96
joel2 <joelpjoseph@gmail.com>
2026-03-11 12:38:26 +0530

    hero caurosal image updated

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -43,7 +43,7 @@ export default function HeroCarousel({ events }) {
 <div
   className="absolute inset-0 bg-cover bg-center"
   style={{
-    backgroundImage: `url(http://127.0.0.1:8000${event.event_image})`,
+    backgroundImage: `url(${event.event_image})`,
     filter: "blur(4px)",
 
     WebkitMaskImage: `
@@ -134,7 +134,7 @@ export default function HeroCarousel({ events }) {
             {/* RIGHT POSTER */}
             <div className="hidden min-[840px]:block">
               <img
-                src={`http://127.0.0.1:8000${event.event_image}`}
+                src={`${event.event_image}`}
                 alt={event.title}
                 className="
                   w-[220px] xl:w-[260px]
